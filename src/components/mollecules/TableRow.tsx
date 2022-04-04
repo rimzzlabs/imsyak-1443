b418fbf8a3c90db0ff1339f373e4e3fd9d0fa9c5
Rizki Maulana Citra <62492410+rizkimcitra@users.noreply.github.com>
2022-04-05 03:04:31 +0700

    style: add GMT WIB(Waktu Indonesia Barat)

--- src/components/mollecules/TableRow.tsx
+++ src/components/mollecules/TableRow.tsx
@@ -9,14 +9,14 @@ export const TableRow: React.FunctionComponent<TableRowProps> = ({ data, index }
   return (
     <tr className='even:bg-theme-2/30 dark:even:bg-theme-3/30'>
       <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{index + 1}</td>
-      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.date}</td>
-      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.imsak}</td>
-      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.subuh}</td>
-      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.dhuha}</td>
-      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.dzuhur}</td>
-      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.ashar}</td>
-      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.maghrib}</td>
-      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.isya}</td>
+      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.date} WIB</td>
+      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.imsak} WIB</td>
+      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.subuh} WIB</td>
+      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.dhuha} WIB</td>
+      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.dzuhur} WIB</td>
+      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.ashar} WIB</td>
+      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.maghrib} WIB</td>
+      <td className='border border-theme-2 dark:border-theme-3 p-2 md:p-4'>{data.isya} WIB</td>
     </tr>
   )
 }
